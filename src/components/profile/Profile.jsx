077b4f35659fcd46a36b9f003638806424c1a321
Venedykt <chewbaccayeti@gmail.com>
2023-03-17 19:21:13 +0200

    Card has been created

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
+
+function Profile(props) {
+    return (
+        <div className={styles.profile}>
+            <div className={styles.description}>
+                <img
+                    src={props.avatar}
+                    alt="User avatar"
+                    className={styles.avatar}
+                />
+                <p className={styles.name}>{props.username}</p>
+                <p className={styles.tag}>@{props.tag}</p>
+                <p className={styles.location}>{props.location}</p>
+            </div>
+
+            <ul className={styles.stats}>
+                <li>
+                    <span className={styles.label}>Followers</span>
+                    <span className={styles.quantity}>
+                        {props.stats.followers}
+                    </span>
+                </li>
+                <li>
+                    <span className={styles.label}>Views</span>
+                    <span className={styles.quantity}>{props.stats.views}</span>
+                </li>
+                <li>
+                    <span className={styles.label}>Likes</span>
+                    <span className={styles.quantity}>{props.stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    stats: {
+        followers: PropTypes.number,
+        views: PropTypes.number,
+        likes: PropTypes.number,
+    },
+};
+
+export default Profile;
