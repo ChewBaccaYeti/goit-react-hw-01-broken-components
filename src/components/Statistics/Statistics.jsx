67083dfcc3bbeca52018001905e3805f48bfffa7
Venedykt <chewbaccayeti@gmail.com>
2023-04-03 21:27:37 +0300

    Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -8,21 +8,19 @@ import {
   Title,
 } from './Statistics.styled';
 
-export const Statistics = ({ title, stats }) => {
-  return (
-    <Container>
-      {title && <Title>{title}</Title>}
-      <List>
-        {stats.map(({ id, label, percentage }) => (
-          <Item key={id}>
-            <Label>{label}</Label>
-            <Percentage>{percentage}%</Percentage>
-          </Item>
-        ))}
-      </List>
-    </Container>
-  );
-};
+export const Statistics = ({ title, stats }) => (
+  <Container>
+    {title && <Title>{title}</Title>}
+    <List>
+      {stats.map(({ id, label, percentage }) => (
+        <Item key={id}>
+          <Label>{label}</Label>
+          <Percentage>{percentage}%</Percentage>
+        </Item>
+      ))}
+    </List>
+  </Container>
+);
 
 Statistics.propTypes = {
   title: PropTypes.string,
